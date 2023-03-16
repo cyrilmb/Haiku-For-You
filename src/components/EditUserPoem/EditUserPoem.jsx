561ca6b5d2310cc35f0e33d57fea5a0579c0851e
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-03-15 21:20:50 -0500

    edit user poem form written, still need to write the POST dispatch and router/redu/saga

---
+++ src/components/EditUserPoem/EditUserPoem.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+function EditUserPoem(poem) {
+
+    //Hooks
+    const dispatch = useDispatch();
+    let [newPoem, setNewPoem] = useState({ title: '', line_1: '', line_2: '', line_3: '' });
+
+    //Handle changes to inputs in form
+    const handleChange = (event, key) => {
+        setNewPoem({ ...newPoem, [key]: event.target.value });
+    };
+
+    //Dispatch input values to be stored in db
+    const addNewPoem = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: 'ADD_POEM',
+            payload: newPoem
+        });
+    };
+
+    return (
+        <div>
+            <form onSubmit={addNewPoem}>
+                <input
+                    type='text'
+                    placeholder={poem.title}
+                    value={newPoem.title}
+                    onChange={(e) => handleChange(e, 'title')}
+                />
+                <input
+                    type='text'
+                    placeholder={poemline_1}
+                    value={newPoem.line_1}
+                    onChange={(e) => handleChange(e, 'line_1')}
+                />
+                <input
+                    type='text'
+                    placeholder={poemline_2}
+                    value={newPoem.line_2}
+                    onChange={(e) => handleChange(e, 'line_2')}
+                />
+                <input
+                    type='text'
+                    placeholder={poemline_3}
+                    value={newPoem.line_3}
+                    onChange={(e) => handleChange(e, 'line_3')}
+                />
+                <input type="submit" value="Submit Your Poem" />
+            </form>
+        </div>
+    );
+}
+
+export default EditUserPoem;
